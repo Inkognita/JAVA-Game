7d9c5200da996c3352cb939d5d8e7c3a2ec78cb0
Inkognita <liepieshov@ucu.edu.ua>
2017-11-10 18:40:57 +0200

    Trying to simplify statements

--- src/main/java/CharacterFactory.java
+++ src/main/java/CharacterFactory.java
@@ -7,16 +7,13 @@ import java.util.Set;
 public class CharacterFactory {
     public static Character createCharacter(){
         Set<Class<? extends Character>> characters = new Reflections().getSubTypesOf(Character.class);
-        Class<? extends Character> cls = (Class<? extends Character>) characters.toArray()[new Random().nextInt(characters.size())];
+        Class<? extends Character> cls = ( Class<? extends Character>) characters.
+                toArray()[new Random().
+                nextInt(characters.size())];
         try {
             return cls.getConstructor().newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
+        {
             e.printStackTrace();
         }
         return null;
--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -10,9 +10,9 @@ public class GameManager {
             return;
         }
         System.out.println("The battle has started!");
-        int temp_hp = 0;
+        int temp_hp;
         int rand_int;
-        ArrayList<Character> characters = new ArrayList<Character>(Arrays.asList(c1, c2));
+        ArrayList<Character> characters = new ArrayList<>(Arrays.asList(c1, c2));
 
         while (c1.isAlive() && c2.isAlive()) {
             rand_int = new Random().nextInt(2);
@@ -21,7 +21,8 @@ public class GameManager {
             temp_hp = c2.getHp();
             c1.kick(c2);
             temp_hp -= c2.getHp();
-            System.out.println(c1.getClass().getName() + (2 - rand_int) + " kicked " + c2.getClass().getName() + (1 + rand_int) + " for " + temp_hp);
+            System.out.println(c1.getClass().getName() + (2 - rand_int) + " kicked " + c2.getClass().getName()
+                    + (1 + rand_int) + " for " + temp_hp);
             System.out.print(c1.getClass().getName() + (2 - rand_int) + ": " + c1.getHp() + "HP; ");
             System.out.print(c2.getClass().getName() + (1 + rand_int) + ": " + c2.getHp() + "HP\n");
 
