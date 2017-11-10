3cbf62b71bc4a015ee3a8409ebc6b51ce75921e0
Inkognita <liepieshov@ucu.edu.ua>
2017-11-10 18:20:08 +0200

    Added pom dependencies and code style checking for Jenkins

--- src/main/java/CharacterFactory.java
+++ src/main/java/CharacterFactory.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.Set;
 
 public class CharacterFactory {
-    public Character createCharacter(){
+    public static Character createCharacter(){
         Set<Class<? extends Character>> characters = new Reflections().getSubTypesOf(Character.class);
         Class<? extends Character> cls = (Class<? extends Character>) characters.toArray()[new Random().nextInt(characters.size())];
         try {
